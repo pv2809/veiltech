8de71783ce872efed16e18e109c7ac4469568b11
pv2809 <vprabhat.v@gmail.com>
2026-01-01 02:53:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Form, Request, Header
+from core.db import get_db
 import mysql.connector
 import time
 from core.sessions.session_manager import invalidate_session, create_session
@@ -26,32 +27,31 @@ def auth_user(
     firebase_uid: str = Form(...),
     phone: str = Form(...)
 ):
-    try:
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+
+    cursor.execute(
+        "SELECT * FROM users WHERE firebase_uid=%s",
+        (firebase_uid,)
+    )
+    user = cursor.fetchone()
+
+    if not user:
         cursor.execute(
-            "SELECT * FROM users WHERE firebase_uid=%s",
-            (firebase_uid,)
+            "INSERT INTO users (firebase_uid, phone) VALUES (%s, %s)",
+            (firebase_uid, phone)
         )
-        user = cursor.fetchone()
+        db.commit()
 
-        if not user:
-            cursor.execute(
-                "INSERT INTO users (firebase_uid, phone) VALUES (%s, %s)",
-                (firebase_uid, phone)
-            )
-            db.commit()
+    cursor.close()
+    db.close()
 
-        # 🔐 Create session using phone as identity
-        session_id = create_session(phone)
+    session_id = create_session(phone)
 
-        return {
-            "message": "login success",
-            "session_id": session_id
-        }
-
-    except Exception as e:
-        print("AUTH ERROR:", e)
-        raise HTTPException(status_code=500, detail="Internal")
-
+    return {
+        "message": "login success",
+        "session_id": session_id
+    }
 # ---------- SECURE ----------
 @app.get("/secure-data")
 def secure_data(
@@ -120,4 +120,5 @@ def logout(request: Request):
         invalidate_session(session_id)
 
     return {"status": "LOGGED_OUT"}
+
 
