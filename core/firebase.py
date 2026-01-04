fa7fbfe8e9917174962393ab512646bd82aec75b
pv2809 <vprabhat.v@gmail.com>
2026-01-05 02:12:34 +0530

    Create firebase.py

---
+++ core/firebase.py
@@ -0,0 +1,8 @@
+import firebase_admin
+from firebase_admin import credentials, auth
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+def verify_firebase_token(id_token: str):
+    return auth.verify_id_token(id_token)
