8a904158865485b03a4ec6bf34fbe2b47b1a8688
pv2809 <vprabhat.v@gmail.com>
2026-01-05 10:11:12 +0530

    Update firebase.py

--- core/firebase.py
+++ core/firebase.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, auth
 
-cred = credentials.Certificate("serviceAccountKey.json")
+cred = credentials.Certificate("service_account")
 firebase_admin.initialize_app(cred)
 
 def verify_firebase_token(id_token: str):
